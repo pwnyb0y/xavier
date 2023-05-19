4355ed4de6b72f203bba816dbef08120f7e4f8e3
jonmarc <j@jonmarc.dev>
2023-05-18 22:38:50 -0700

    Makefile, .gitignore and some other cleanup

--- Makefile
+++ Makefile
@@ -1,3 +1,59 @@
+# Go parameters
+GO := go
+GOBUILD := $(GO) build
+GOCLEAN := $(GO) clean
+GOTEST := $(GO) test
+GOGET := $(GO) get
+
+# Directories
+CMD_DIR := cmd
+PROTO_DIR := proto
+GEN_DIR := gen/go
+BIN_DIR := bin
+
+# Binary names
+CLIENT_BINARY := $(BIN_DIR)/client
+SERVER_BINARY := $(BIN_DIR)/server
+
+.PHONY: all build test clean generate get-deps run-client run-server
+
+all: build
+
+build: build-client build-server
+
+build-client:
+	@echo "Building client..."
+	@cd $(CMD_DIR)/client && $(GOBUILD) -o ../../$(CLIENT_BINARY)
+
+build-server:
+	@echo "Building server..."
+	@cd $(CMD_DIR)/server && $(GOBUILD) -o ../../$(SERVER_BINARY)
+
+test:
+	@echo "Running tests..."
+	@$(GOTEST) -v ./...
+
+clean:
+	@echo "Cleaning..."
+	@$(GOCLEAN)
+	@rm -f $(CLIENT_BINARY) $(SERVER_BINARY)
+
+generate:
+	@echo "Generating Go code from protocol buffers..."
+	@cd $(PROTO_DIR) && protoc -I xavier/v1/ xavier/v1/xavier.proto --go_out=plugins=grpc:../$(GEN_DIR)
+
+get-deps:
+	@echo "Getting dependencies..."
+	@$(GOGET) ./...
+
+run-client: build-client
+	@echo "Running client..."
+	@$(CLIENT_BINARY)
+
+run-server: build-server
+	@echo "Running server..."
+	@$(SERVER_BINARY)
+
 tidy:
 	go mod tidy
 	go mod vendor
--- cmd/server/xavier_service.go
+++ cmd/server/xavier_service.go
@@ -3,10 +3,11 @@ package main
 import (
 	"context"
 	"encoding/json"
-	pb "github.com/pwnyb0y/xavier/gen/go/proto/xavier/v1"
 	"io"
 	"log"
 	"net/http"
+
+	pb "github.com/pwnyb0y/xavier/gen/go/proto/xavier/v1"
 )
 
 // XavierServiceServer is a server for the Xavier service.
@@ -14,12 +15,31 @@ type XavierServiceServer struct {
 	*pb.UnimplementedXavierServer
 }
 
-// OpenAIModelsResponse represents the structure of the response from the OpenAI API.
-type OpenAIModelsResponse struct {
-	Models []struct {
-		ID   string `json:"id"`
-		Name string `json:"name"`
-	} `json:"models"`
+// OpenAIModel represents the structure of a model in the OpenAI API response.
+type OpenAIModel struct {
+	ID          string             `json:"id"`
+	Object      string             `json:"object"`
+	Created     int64              `json:"created"`
+	OwnedBy     string             `json:"owned_by"`
+	Permissions []OpenAIPermission `json:"permissions"`
+	Root        string             `json:"root"`
+	Parent      string             `json:"parent"`
+}
+
+// OpenAIPermission represents the structure of a permission in the OpenAI API response.
+type OpenAIPermission struct {
+	ID                 string `json:"id"`
+	Object             string `json:"object"`
+	Created            int64  `json:"created"`
+	AllowCreateEngine  bool   `json:"allow_create_engine"`
+	AllowSampling      bool   `json:"allow_sampling"`
+	AllowLogprobs      bool   `json:"allow_logprobs"`
+	AllowSearchIndices bool   `json:"allow_search_indices"`
+	AllowView          bool   `json:"allow_view"`
+	AllowFineTuning    bool   `json:"allow_fine_tuning"`
+	Organization       string `json:"organization"`
+	Group              string `json:"group"`
+	IsBlocking         bool   `json:"is_blocking"`
 }
 
 // GetModels reaches out to the OpenAI API and gets a list of all available models.
@@ -38,7 +58,6 @@ func (s *XavierServiceServer) GetModels(ctx context.Context, req *pb.GetModelsRe
 		log.Printf("failed to make request to OpenAI API: %v", err)
 		return nil, err
 	}
-	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
@@ -46,30 +65,45 @@ func (s *XavierServiceServer) GetModels(ctx context.Context, req *pb.GetModelsRe
 		return nil, err
 	}
 
-	log.Printf("response body: %s", body)
+	defer func() {
+		if closeErr := resp.Body.Close(); closeErr != nil {
+			log.Printf("failed to close response body: %v", closeErr)
+		}
+	}()
 
-	var models []*pb.Model
-	err = json.Unmarshal(body, &models)
+	var openAIResponse struct {
+		Models []OpenAIModel `json:"models"`
+	}
+	err = json.Unmarshal(body, &openAIResponse)
 	if err != nil {
 		log.Printf("failed to unmarshal response body: %v", err)
 		return nil, err
 	}
 
-	for _, model := range models {
+	var models []*pb.Model
+	for _, model := range openAIResponse.Models {
 		var permissions []*pb.Permission
 		for _, perm := range model.Permissions {
 			permissions = append(permissions, &pb.Permission{
+				Id:                 perm.ID,
+				Object:             perm.Object,
+				Created:            perm.Created,
 				AllowCreateEngine:  perm.AllowCreateEngine,
 				AllowSampling:      perm.AllowSampling,
 				AllowLogprobs:      perm.AllowLogprobs,
 				AllowSearchIndices: perm.AllowSearchIndices,
 				AllowView:          perm.AllowView,
 				AllowFineTuning:    perm.AllowFineTuning,
+				Organization:       perm.Organization,
+				Group:              perm.Group,
+				IsBlocking:         perm.IsBlocking,
 			})
 		}
 		models = append(models, &pb.Model{
-			Id:          model.Id,
+			Id:          model.ID,
 			Permissions: permissions,
+			Root:        model.Root,
+			Parent:      model.Parent,
 		})
 	}
 
